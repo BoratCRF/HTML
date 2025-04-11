0bb7b6cd7f6dfa9b5f528aa1608b29d7dc0d033c
BoratCRF <vinicius.lino16snts@gmail.com>
2025-04-11 16:45:11 -0300

    11/04

--- hdg/home/models.py
+++ hdg/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Inscritos(models.Model):
+    id_inscrito = models.AutoField(primary_key=True)
+    emailsub = models.TextField(max_length=100)
--- hdg/home/views.py
+++ hdg/home/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from home.models import Inscritos
+
 # Create your views here.
 def home(request):
     contexto = {
-        'titulo' : 'High-Dream Games | Home'
+        'titulo' : 'High-Dream Games | Home',
+        'inscritos': Inscritos.objects.all(),
     }
     return render(
         request,
         'home/index.html',
         contexto,
     )
+
+def gravarsub(request):
+    novo_inscrito = Inscritos()
+    novo_inscrito.emailsub = request.POST.get("emailsub")
+    novo_inscrito.save()
+
+    return home(request)
